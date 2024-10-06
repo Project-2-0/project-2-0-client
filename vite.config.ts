214bc4346e4224b18087b4b574cc31b9f1fecc29
Miloslav Jezek <milo.jezek@gmail.com>
2024-10-06 21:04:25 +0200

    Add basic dependencies for routing, testing and styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+		globals: true,
+    setupFiles: './src/tests/setup.ts', 
+		include: ['src/**/*.test.{js,ts,jsx,tsx}']
+  }
 });
